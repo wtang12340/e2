e5a0ce04ef73c218af73acae0552a505fa80f825
William Tang <wtang12340@gmail.com>
2021-09-20 03:19:59 +0000

    test

--- practice/bank.php
+++ practice/bank.php
@@ -6,20 +6,23 @@ $penny_value = .01;
 $nickel_value = .05;
 $dime_value = .10;
 $quarter_value = .25;
+$halfdollar_value = .5;
 
 # Define 4 more variables, which will each
 # represent how many of each coin is in the bank
-$pennies = 100;
-$nickels = 25;
-$dimes = 100;
-$quarters = 34;
+$pennies = 300;
+$nickels = 5;
+$dimes = 0;
+$quarters = 125;
+$halfdollars = 33;
 
 # Add up how much money is in the piggy bank
-$total = ($pennies * $penny_value) + ($nickels * $nickel_value) + ($dimes * $dime_value) + ($quarters * $quarter_value);
+$total = ($pennies * $penny_value) + ($nickels * $nickel_value) + ($dimes * $dime_value) + ($quarters * $quarter_value) + ($halfdollars * $halfdollar_value);
 
 ?>
 <!DOCTYPE html>
 <html lang='en'>
+
 <head>
 
     <title>PHPiggy Bank</title>
@@ -30,11 +33,14 @@ $total = ($pennies * $penny_value) + ($nickels * $nickel_value) + ($dimes * $dim
 
 <body>
 
-    <img alt='PHPiggy Bank Logo' src='https://s3.amazonaws.com/making-the-internet/php-piggy-bank-logo@2x.png' style='width:202px;'>
+    <img alt='PHPiggy Bank Logo' src='https://s3.amazonaws.com/making-the-internet/php-piggy-bank-logo@2x.png'
+        style='width:202px;'>
 
     <p>
         You have $<?php echo $total; ?> in your piggy bank.
+        You have <?php echo $quarter; ?> quarters in your bank.
     </p>
 
 </body>
+
 </html>
